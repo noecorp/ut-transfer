7b58cba43fdbccf68218c8ad1538956398fcf46f
mtchaparova <maria.tchaparova@softwaregroup-bg.com>
2017-10-19 11:14:42 +0300

    fix: MCTUNE-4907 - remove code for ATM and ledger

--- api/sql/schema/750$transfer.idle.execute.sql
+++ api/sql/schema/750$transfer.idle.execute.sql
@@ -8,42 +8,32 @@ DECLARE
     @opcode varchar(20)
 BEGIN TRY
     -- forward stored
-    UPDATE
-        [transfer].[transfer]
-    SET
-        expireTime = CASE
-            WHEN 5>isnull(retryCount,0) THEN DATEADD(SECOND, 30, GETDATE())
-            WHEN 10>retryCount THEN DATEADD(SECOND, 60, GETDATE())
-            WHEN 15>retryCount THEN DATEADD(MINUTE, 30, GETDATE())
-            WHEN 20>retryCount THEN DATEADD(HOUR, 1, GETDATE())
-        END,
-        @txid = transferId,
-        retryCount = ISNULL(retryCount, 0) + 1,
-        retryTime = GETDATE(),
-        @mtid = '200',
-        @opcode = 'forward'
-    WHERE
-        transferId IN (
-            SELECT TOP 1
-                t.transferId
-            FROM
-                [transfer].[transfer] t
-            JOIN
-                [transfer].[partner] p ON p.partnerId = t.issuerId AND p.mode in ('online') AND p.port = @issuerPort
-            WHERE
-                t.issuerTxState IN (8, 11, 13, 14) AND
-                ISNULL(t.acquirerTxState, CASE t.channelType WHEN 'POS' THEN 2 else 0 END) IN (2) AND
-                t.reversed = 0 AND
-                20 > ISNULL(t.retryCount,0) AND
-                GETDATE() >= t.expireTime AND
-                t.transferDateTime > DATEADD(DAY, -1, GETDATE()) AND
-                t.channelType IN ('ATM','POS')
-            ORDER
-                BY t.expireTime, t.transferId
-        )
+    DECLARE @transferId BIGINT
+    SET  @transferId = 
+    (
+        SELECT TOP 1
+            t.transferId
+        FROM
+            [transfer].[transfer] t
+        JOIN
+            [transfer].[partner] p ON p.port = @issuerPort AND 
+            ((
+                (t.issuerTxState = 2 AND ISNULL(t.acquirerTxState, 0) IN (0 , 3, 4, 5) AND p.mode = 'online') OR                --tx succeeded at issuer during online but failed at acquirer and current mode is online
+                (t.issuerTxState = 8 AND ISNULL(t.acquirerTxState, 0) IN (0 , 3, 4, 5) AND p.mode IN ('online', 'offline')) OR --tx succeeded at issuer during offline but failed at acquirer and current mode is online/offline
+                (ISNULL(t.merchantTxState, 0) IN (1, 3)) OR                                                               --tx failed at merchant (succeeded at issuer implicitly)
+                (t.issuerTxState IN (1,4) AND p.mode = 'online') OR                                                    --tx timed out at issuer during online and current mode is online
+                (t.issuerTxState IN (7,9) AND p.mode IN ('online', 'offline'))                                        --tx timed out during offline and current mode is online/offline
+            ) AND p.partnerId = t.issuerId) 
+        WHERE
+            t.reversed = 0 AND
+            20 > ISNULL(t.expireCount, 0) AND
+            GETDATE() >= t.expireTime AND
+            t.transferDateTime > DATEADD(DAY, -1, GETDATE())
+        ORDER
+            BY t.expireTime, t.transferId   )
 
-    IF @txid IS NULL -- reversals
-    BEGIN
+    IF @transferId IS NOT NULL
+    BEGIN 
         UPDATE
             [transfer].[transfer]
         SET
@@ -58,74 +48,7 @@ BEGIN TRY
             @mtid = '420',
             @opcode = 'reverse'
         WHERE
-            transferId IN (
-                SELECT TOP 1
-                    t.transferId
-                FROM
-                    [transfer].[transfer] t
-                JOIN
-                    [transfer].[partner] p ON p.port = @issuerPort AND (((
-                       (t.issuerTxState = 2 AND ISNULL(t.acquirerTxState, 0) IN (0 , 3, 4, 5) AND p.mode = 'online') OR                --tx succeeded at issuer during online but failed at acquirer and current mode is online
-                       (t.issuerTxState = 8 AND ISNULL(t.acquirerTxState, 0) IN (0 , 3, 4, 5) AND p.mode IN ('online', 'offline')) OR --tx succeeded at issuer during offline but failed at acquirer and current mode is online/offline
-                       (ISNULL(t.merchantTxState, 0) IN (1, 3)) OR                                                               --tx failed at merchant (succeeded at issuer implicitly)
-                       (t.issuerTxState IN (1,4) AND p.mode = 'online') OR                                                    --tx timed out at issuer during online and current mode is online
-                       (t.issuerTxState IN (7,9) AND p.mode IN ('online', 'offline'))                                        --tx timed out during offline and current mode is online/offline
-                    ) AND p.partnerId = t.issuerId) OR ((
-                       (t.ledgerTxState = 2 AND t.issuerTxState = 3) OR
-                       (t.ledgerTxState IN (1,4) AND p.mode = 'online') OR                                                    --tx timed out at issuer during online and current mode is online
-                       (t.ledgerTxState IN (7,9) AND p.mode IN ('online', 'offline'))                                        --tx timed out during offline and current mode is online/offline
-                    ) AND  p.partnerId = t.ledgerId))
-                WHERE
-                    t.reversed = 0 AND
-                    20 > ISNULL(t.expireCount, 0) AND
-                    GETDATE() >= t.expireTime AND
-                    t.transferDateTime > DATEADD(DAY, -1, GETDATE()) AND
-                    t.channelType IN ('ATM', 'web')
-                ORDER
-                    BY t.expireTime, t.transferId
-            )
-    END
-
-    IF @txid IS NULL -- reversals of expired pending
-    BEGIN
-        UPDATE
-            [transfer].[transfer]
-        SET
-            expireTime = CASE
-                WHEN 5 > ISNULL(expireCount,0) THEN DATEADD(SECOND, 30, GETDATE())
-                WHEN 10 > expireCount THEN DATEADD(SECOND, 60, GETDATE())
-                WHEN 15 > expireCount THEN DATEADD(MINUTE, 30, GETDATE())
-                WHEN 20 > expireCount THEN DATEADD(HOUR, 1, GETDATE())
-            END,
-            @txid = transferId,
-            expireCount = ISNULL(expireCount, 0) + 1,
-            @mtid = '200',
-            @opcode = 'reverse'
-        WHERE
-            issuerTxState IN (2, 12) AND
-            reversed = 0 AND
-            20 > isnull(expireCount, 0) AND
-            GETDATE() >= expireTime AND
-            transferId IN (
-                SELECT TOP 1
-                    d.transferId
-                FROM
-                    [transfer].[pending] cc
-                JOIN
-                    [transfer].[transfer] d ON d.transferId = cc.pullTransactionId
-                JOIN
-                    [transfer].[partner] p ON p.partnerId = d.issuerId AND p.port = @issuerPort
-                WHERE
-                    cc.status IS NULL
-                    AND cc.expireTime < GETDATE()
-                    AND d.issuerTxState IN (2,12)
-                    AND d.reversed = 0
-                    AND 20 > ISNULL(d.expireCount, 0)
-                    AND GETDATE() >= d.expireTime
-                    AND d.channelType IN ('ATM')
-                ORDER BY
-                    d.expireTime, d.transferId
-            )
+            transferId = @transferId 
     END
 
     IF @txid IS NULL
